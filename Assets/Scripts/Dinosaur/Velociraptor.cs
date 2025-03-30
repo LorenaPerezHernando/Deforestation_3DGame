06c24451c77fbcc1ed4805c81e34c58d7999097e
LorenaPerezHernando <lorenaperezhernando@gmail.com>
2025-03-30 15:52:46 +0200

    Velociraptor correct rotation

--- Assets/Scripts/Dinosaur/Velociraptor.cs
+++ Assets/Scripts/Dinosaur/Velociraptor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Deforestation.Dinosaurus;
 using UnityEngine;
+using UnityEngine.AI;
 namespace Deforestation.Dinosaurus
 {
 
@@ -11,47 +12,66 @@ namespace Deforestation.Dinosaurus
         #region Fields
         [SerializeField] private float _distanceDetection = 50;
         [SerializeField] private float _attackDistance = 7;
-        private Vector3 _playerPosition => GameController.Instance.HealthSystem.transform.position;
+        private Vector3 _playerPosition => GameController.Instance.Inventory.transform.position;
 
         private bool _chase;
         private bool _attack;
-        private bool _dead;
 
-        HealthSystem _healthSystem;
+        [SerializeField] private float _attackTime = 2;
+        [SerializeField] private float _attackDamage = 5;
+        private float _attackColdDown;
+
+
         #endregion
 
         #region Unity Callbacks
-        
 
-       
+
+
         void Update()
         {
+           
             //Idle
-            if(!_chase && !_attack &&  !_dead && Vector3.Distance(transform.position, _playerPosition) < _distanceDetection)
-            {
-                Idle();
-                return; 
-            }
-            //Chase
-            if (_chase && Vector3.Distance(transform.position, _playerPosition ) < _distanceDetection)
+            if (!_chase && !_attack && Vector3.Distance(transform.position, _playerPosition) < _distanceDetection)
             {
                 ChasePlayer();
-                return; 
-            }
-            if (_chase && Vector3.Distance(transform.position, _playerPosition) > _distanceDetection)
-            {
-                Idle(); 
                 return;
             }
 
-            if (_attack && Vector3.Distance(transform.position, _playerPosition) > _attackDistance)
+            //Chase
+            if (_chase)
             {
-                ChasePlayer();
-                return;
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(_playerPosition, out hit, _attackDistance, 1))
+                    _agent.SetDestination(hit.position);
             }
+
             if (_chase && Vector3.Distance(transform.position, _playerPosition) < _attackDistance)
             {
                 Attack();
+                return;
+            }
+            if (_chase && Vector3.Distance(transform.position, _playerPosition) > _distanceDetection)
+            {
+                Idle();
+                return;
+            }
+
+            //Attack
+            if (_attack)
+            {
+                //Atack damage to player
+                _attackColdDown -= Time.deltaTime;
+                if (_attackColdDown <= 0)
+                {
+                    print("Haciendo daño al player");
+                    _attackColdDown = _attackTime;
+                    GameController.Instance.HealthSystem.TakeDamage(_attackDamage);
+                }
+            }
+            if (_attack && Vector3.Distance(transform.position, _playerPosition) > _attackDistance)
+            {
+                ChasePlayer();
                 return;
             }
 
@@ -61,21 +81,24 @@ namespace Deforestation.Dinosaurus
         #region Private Methods
         private void Idle()
         {
+            
             _anim.SetBool("Run", false);
             _anim.SetBool("Attack", false);
             _agent.isStopped = true;
             _chase = false;
             _attack = false;
-            _dead = false;
+
         }
         private void ChasePlayer()
-        {
+        {       
+
             _anim.SetBool("Run", true);
             _anim.SetBool("Attack", false); 
             _agent.SetDestination(_playerPosition);
+            
             _chase = true;
             _attack = false;
-            _dead = false;
+           
         }
 
         private void Attack()
@@ -85,7 +108,17 @@ namespace Deforestation.Dinosaurus
             _agent.isStopped = true;
             _chase = false;
             _attack = true;
-            _dead = false;
+        
+        }
+
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, _distanceDetection);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _attackDistance);
         }
 
         #endregion
