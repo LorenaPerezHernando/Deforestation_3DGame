768a9e05eb802555b8d4748d9b09acef59762908
LorenaPerezHernando <lorenaperezhernando@gmail.com>
2025-06-04 19:23:39 +0200

    Build Succesful

--- Assets/Scripts/Network/NetworkController.cs
+++ Assets/Scripts/Network/NetworkController.cs
@@ -9,11 +9,14 @@ namespace Deforestation.Network
 
     public class NetworkController : MonoBehaviourPunCallbacks
     {
-        [SerializeField] private UINetwork _ui; 
-        void Start()
-        {
+        [SerializeField] private UINetwork _ui;
+        [SerializeField] private Transform[] _spawnPoints;
+        private static bool[] _spawnPointsTaken;
+         void Start()
+         {
             PhotonNetwork.ConnectUsingSettings();
-        }
+   
+         }
 
         public override void OnConnectedToMaster()
         {
@@ -21,11 +24,45 @@ namespace Deforestation.Network
         }
         public override void OnJoinedRoom()
         {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                _spawnPointsTaken = new bool[_spawnPoints.Length];
+                photonView.RPC("RPC_SpawnPlayer", RpcTarget.MasterClient);
+            }
             //Player tiene que estar en la carpeta llamada "Resources"
-            PhotonNetwork.Instantiate("PlayerMultiplayer",new Vector3(1747f, 126f, 360f), Quaternion.identity);
-            PhotonNetwork.Instantiate("TheMachineMultiplayer", new Vector3(1700f, 126f, 360f), Quaternion.identity);
+            
+            
             _ui.LoadingComplete();
         }
+
+        [PunRPC]
+        void RPC_SpawnPlayer()
+        {
+            int index = -1;
+            for(int i = 0;  i < _spawnPoints.Length; i++)
+            {
+                if (!_spawnPointsTaken[i])
+                {
+                    //Cogemos la primera pos no cogida
+                    index = i;
+                    _spawnPointsTaken[i] = true;
+                    break;
+                }
+            }
+            if(index != -1)
+            {
+                Transform spawnPoint = _spawnPoints[index];
+                PhotonNetwork.Instantiate("PlayerMultiplayer", spawnPoint.position, Quaternion.identity);
+                PhotonNetwork.Instantiate("TheMachineMultiplayer", spawnPoint.position, Quaternion.identity);
+            }
+            else
+            {
+                Debug.LogError("No hay puntos de spawn disponibles");
+            }
+        }
+        
+
+        
     }
 
 }
